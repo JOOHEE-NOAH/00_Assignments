1df66dc965db825f2e4267df535e7a401e9bdb20
JOOHEE-NOAH <joohee195@gmail.com>
2022-01-31 16:43:22 +0900

    Test1~6 commit

--- src/step00_과제/Test1.java
+++ src/step00_과제/Test1.java
@@ -1,7 +1,6 @@
 package step00_과제;
 /*
  * String a = "100"; String b = "200"; a 와 b의 값을 형변환해서 두수의 곱을 구하시오
- * 
  * --출력-- 100 * 200 = 20000
  */
 public class Test1 {
@@ -14,6 +13,7 @@ public class Test1 {
 		int b1=Integer.parseInt(b);
 		
 		System.out.println(a+" * "+b+" = "+a1*b1);
+		
 	}
 
 }
