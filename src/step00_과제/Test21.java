8cd2f87ad9665a792eda6edd0a219cd10302e173
JOOHEE-NOAH <joohee195@gmail.com>
2022-01-31 22:57:36 +0900

    Test15 commit

---
+++ src/step00_과제/Test21.java
@@ -0,0 +1,29 @@
+package step00_과제;
+
+/*[문제 21] 아래 배열의 값 중 홀수방의 합과 짝수방의 합을 구하시오(단일 for문)
+int[] num={1,5,2,7,3,9,10,4,6,8};
+
+--출력--
+홀수방의 합= 33
+짝수방의 합= 22
+
+*/public class Test21 {
+
+	public static void main(String[] args) {
+		int odd=0, even=0;
+		int[] num={1,5,2,7,3,9,10,4,6,8};
+			
+			
+		for(int i=0;i<=num.length;i+=2) {
+		
+			switch(i%2){
+			color[i];
+				case 0: odd+=i;
+				break;
+				default :even+=i;
+			}
+			System.out.println(color[i]);
+		}
+	}
+
+}
