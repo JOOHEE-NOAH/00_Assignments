81a6dbc7d638d13fc20fa1610bd3ecdce5229cf8
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-01 19:20:00 +0900

    Test22 commit

--- src/step00_과제/Test22.java
+++ src/step00_과제/Test22.java
@@ -17,7 +17,7 @@ public class Test22 {
 	public static void main(String[] args) {
 		String[] color={"red","green","blue","yellow","pink"};
 		
-		for(int i=0;i<=color.length;i++) {
+		for(int i=0;i<color.length;i++) {
 			System.out.println(color[i]+"는(은) "+color[i].length()+"글자 입니다");		
 		}
 	}
