060b0ad4a548a98bb01c39492805f3ebaa3c3b8e
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-02 01:13:03 +0900

    Test23 commit

--- src/step00_과제/Test23.java
+++ src/step00_과제/Test23.java
@@ -12,7 +12,7 @@ public class Test23 {
 	public static void main(String[] args) {
 		int sum=0;
 		
-		for(int i=1;i<=10;i++) {
+		for(int i=1;i<=10;i++) {  
 			sum=sum+i;
 			System.out.print(i);
 			if(i<=9) {
