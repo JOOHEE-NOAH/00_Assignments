e38decf2359d6d5396263c4ea07c4afac7f97404
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-02 14:05:10 +0900

    Test29 commit

--- src/step00_과제/Test29.java
+++ src/step00_과제/Test29.java
@@ -1,10 +1,13 @@
 package step00_과제;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*[문제29] 다음 프로그램을 작성하시오   
 (1) BufferedReader를 이용하여 콘솔에서 입력받는다
 (2) 이름, 나이....등 각각 배열을 3개씩 만들어서 입력 받는다
-      String[] name=new String[3];
-      int[] age=new int[3];
+      
 
 
 [실행결과]
@@ -22,8 +25,24 @@ package step00_과제;
 개나리   52세*/
 public class Test29 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String[] name=new String[3];
+		int[] age=new int[3];
+		
+		for(int i=0;i<3;i++) {
+			System.out.print("이름을 입력하시오 : ");
+			name[i]=br.readLine();
+			System.out.print("나이를 입력하시오 : ");
+			age[i]=Integer.parseInt(br.readLine());
+			
+		}
+	
+		System.out.println();
+		
+		for(int i=0;i<3;i++) {
+			System.out.println(name[i]+"\t"+age[i]+"세");
+		}
 	}
 
 }
