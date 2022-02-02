2458a1414add58c1afedc07b323bff9eee871c61
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-02 13:54:54 +0900

    Test28 commit

--- src/step00_과제/Test28.java
+++ src/step00_과제/Test28.java
@@ -1,5 +1,9 @@
 package step00_과제;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*[문제28] 다음 프로그램을 작성하시오
 (1)Argument를 통해 숫자를 입력받은후 입력숫자만큼 정수형배열을 만든다
 (2)콘솔에서 각각 배열에 데이타를 입력 받는다
@@ -17,8 +21,26 @@ score[4]를 입력하시오  : 100.0
 평균 : 56.45               <--- 소수점 아래 둘째자리까지 구하시오*/
 public class Test28 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		double n;
+		int num=Integer.parseInt(args[0]);
+		double sum=0;
+		double avg=0;
+	for(int i=0;i<num;i++) {
+		System.out.print("score["+i+"]를 입력하시오 : ");
+		n=Double.parseDouble(br.readLine());
+		
+		sum+=n;
+		avg=sum/num;
+	}	
+		
+		System.out.printf("평균 : %.2f",avg);
+		
+			
+			
+			
+		 
 	}
 
 }
