f300732329b5582566ec096d741b894ab4479217
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-04 17:31:40 +0900

    i+=2에서 i++로 수정 후 if문 추가

--- src/step00_과제/Test21.java
+++ src/step00_과제/Test21.java
@@ -1,5 +1,7 @@
 package step00_과제;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
+
 /*[문제 21] 아래 배열의 값 중 홀수방의 합과 짝수방의 합을 구하시오(단일 for문)
 int[] num={1,5,2,7,3,9,10,4,6,8};
 
@@ -13,18 +15,20 @@ int[] num={1,5,2,7,3,9,10,4,6,8};
 		int odd=0, even=0;
 		int[] num={1,5,2,7,3,9,10,4,6,8};
 			
-			
-		for(int i=0;i<=num.length;i+=2) {
 		
-			switch(i%2){
-			
-				case 0: odd+=i;
-				break;
-				default :even+=i;
+		
+		for(int i=0;i<num.length;i++) {
+			if(i%2==0) {
+				even+=num[i];
+			}else {
+				odd+=num[i];
 			}
-			System.out.println(odd);
-			System.out.println(even);
-		}
+			}
+			
+			System.out.println("홀수방의 합= "+odd);
+			System.out.println("짝수방의 합= "+even);
+				
+		
 	}
 
 }
