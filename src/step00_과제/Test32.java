fb01a333d3dee5be34d73ffdb3a13e132566ca7f
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-05 17:22:21 +0900

    Test32 initial commit

--- src/step00_과제/Test32.java
+++ src/step00_과제/Test32.java
@@ -20,9 +20,23 @@ package step00_과제;
 사다리꼴의넓이 : 24.5
 */
 public class Test32 {
-
+	public final double PI=3.141592;
+	
+	public double area(int a) {
+		return a*a*PI;
+	}
+	
+	public double area(int a, int b) {
+		return a*b;
+	}
+	public double area(int a,int b, int c) {
+		return (double)(a+b)*c/2;
+	}
 	public static void main(String[] args) {
-
+		Test32 ob=new Test32();
+		System.out.println("원의 넓이:" + ob.area(3));
+		System.out.println("사각형의넓이:" + ob.area(4,5));
+		System.out.println("사다리꼴의넓이:" + ob.area(3,4,7));
 	}
 
 }
