21dab05b97512aa6d06222ac31f3e097a129ca4e
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-05 17:42:24 +0900

    Test33 initial commit

--- src/step00_과제/Test33.java
+++ src/step00_과제/Test33.java
@@ -25,10 +25,59 @@ package step00_과제;
 저  자 : 송JP
 가  격 : 35000원
 */
+class BookShop{
+	private String bname;
+	private String  author;
+	private int price;
+	
+	public void setBook(String bname, String author, int price) {
+		this.bname=bname;
+		this.author=author;
+		this.price=price;
+	}
+	public void viewBook() {
+		System.out.println("책이름 : "+bname);
+		System.out.println("저  자 : "+author);
+		System.out.println("가  격 : "+price+"원");
+		
+	}
+	public String getBname() {
+		return bname;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
+
+//3) setBname("JSP잡기"),  setAuthor("송JP"), setPrice(3500)로 값을 대입한후
+  //   getBname(), getAuthor(), getPrice()로 출력한다
+
 public class Test33 {
 
 	public static void main(String[] args) {
-
+		BookShop ob=new BookShop();
+		ob.setBook("자바완성","황완성",25000);
+		ob.viewBook();
+		ob.setBname("JSP잡기");
+		ob.setAuthor("송JP");
+		ob.setPrice(3500);
+		System.out.println();
+		System.out.println("책이름 : "+ob.getBname());
+		System.out.println("저  자 : "+ob.getAuthor());
+		System.out.println("가  격 : "+ob.getPrice()+"원");
 	}
 
 }
